3a79e8ae9bdd1aa6865baa1ebf3e5a9b3b4aa392
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-26 01:04:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,14 +58,11 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do | season, holiday|
      puts "#{season.capitalize}:"
      holiday.each do |holiday, item|
-       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+       puts "  #{holiday.to_s.split("_").map 
+       {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
      end
   end 
 end
-
-end
-  
-  
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
