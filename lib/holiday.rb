cdd5beafbe2e48d694d2652334195497dbdbd9a0
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-26 00:57:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,7 @@ def all_winter_holiday_supplies(holiday_hash)
   arr2.flatten
 end
   
-  def all_supplies_in_holidays(holiday_hash)
-  end
+  
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
