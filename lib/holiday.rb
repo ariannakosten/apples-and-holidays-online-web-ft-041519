6b88323a65997341ff1ff31da58f428ad852ee79
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-26 01:00:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
       holiday if supply.include?("BBQ")
-      holiday_hash.flatten.compact
+      .flatten.compact
     end 
   end
 end
